b9e67905b8ca76010899571f5cf5592c8ab60eb3
manthan-sectin <malimanthan996@gmail.com>
2025-11-19 10:35:12 +0530

    This is UAT changes

--- tests/TestCase1.spec copy.js
+++ tests/TestCase1.spec copy.js
@@ -11,3 +11,12 @@ test('has title', async ({ page }) => {
   const textContent=await page.locator("//option[text()='Euro']").textContent();
   await expect(textContent).toContain('Euro');
 });
+
+test.only('has title', async ({ page }) => {
+  await page.goto('https://demo.nopcommerce.com/');
+  await page.locator('#customerCurrency').click();
+  await page.selectOption('#customerCurrency',{index: 1});
+  await page.selectOption('#customerCurrency',{index: 1});
+  const textContent=await page.locator("//option[text()='Euro']").textContent();
+  await expect(textContent).toContain('Euro');
+});
